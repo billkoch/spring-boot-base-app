120c519be2bb1869f8bce9a0f139e3ae7f6fe7b3
Stuart Ingram <ingrams2@upmc.com>
2015-12-09 15:47:46 -0500

    Working test.  Time to test restAssured & flyway-test

--- src/test/java/srai/integration/ThoughtAPITests.java
+++ src/test/java/srai/integration/ThoughtAPITests.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,7 +18,7 @@ import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
 import srai.Application;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=Application.class)
+@SpringApplicationConfiguration(Application.class)
 @WebAppConfiguration
 public class ThoughtAPITests {
 
