8194775ff7c50bb6e1c6e893e6d6974612e4c4fe
Stuart Ingram <ingrams2@upmc.com>
2015-12-10 07:23:52 -0500

    Not working (save point)

--- src/test/java/srai/integration/ThoughtAPITests.java
+++ src/test/java/srai/integration/ThoughtAPITests.java
@@ -2,13 +2,18 @@ package srai.integration;
 
 import static com.jayway.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertNotNull;
 
+import org.flywaydb.test.junit.FlywayTestExecutionListener;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -20,6 +25,9 @@ import srai.Application;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(Application.class)
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+	FlywayTestExecutionListener.class })
+@IntegrationTest
 @WebAppConfiguration
 public class ThoughtAPITests {
 
@@ -36,7 +44,10 @@ public class ThoughtAPITests {
 	}
 
 	@Test
+	//	@FlywayTest
+	//	@FlywayTest(locationsForMigrate = {"db/data"})
 	public void createsDataSource() {
+		assertNotNull("foo");
 		when().
 		get("/people/1").
 		then().
